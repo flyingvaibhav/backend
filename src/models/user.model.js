b968cd0dad747a927c2f156740f78b30822faebf
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-11-15 23:57:01 +0530

    Add user schema for user data management

---
+++ src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose ,{Schema} from "mongoose";
+const userSchema = new Schema({
+    username: {
+        type: String,
+         required: true,
+          unique: true,
+        lowercase: true,
+         trim: true,
+            
+        },
+    email: {
+        type: String,
+         required: true,
+          unique: true,
+          lowercase: true,
+           trim: true 
+    },
+    fullname: {
+        type: String,
+         required: true,
+            trim: true,
+            index: true
+    },
+    avatar: {
+        type: String,// cloudinary url
+        required: true
+    },
+    coverImage: {
+        type: String, // cloudinary url
+    },
+    watchHistory: {
+        type:}
