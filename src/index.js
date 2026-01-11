88ce9724ee9b415ad8227b91c1ca0d486b2a9c62
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-01-11 21:14:40 +0530

    Fix formatting and improve structure in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config({
-    path: './.env'
-});
+dotenv.config({ path: "./.env" });
+
 connectDB()
-.then(() => {
-    applist.listen(process.env.PORT || 8000 ,() => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+    .then(() => {
+        const port = process.env.PORT || 8000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MONGODB connect FAILED !!:", err);
     });
-
-})
-.catch((err) => {
-    console.error("MONGODB connect FAILED !!:", err);
-});
