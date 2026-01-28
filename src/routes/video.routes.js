1e0c3d4143473574f6edc76c2c410e00d0808abd
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-01-28 22:21:13 +0530

    Add video upload middleware to video creation route

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { listVideos, createVideo, incrementView } from '../controllers/video.controller.js';
+import { uploadVideoAndThumbnail } from '../middlewares/upload.js';
 
 const router = Router();
 
 router.get('/', listVideos);
-router.post('/', createVideo);
+router.post('/', uploadVideoAndThumbnail, createVideo);
 router.post('/:id/view', incrementView);
 
 export default router;
