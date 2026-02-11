4c0a54da9bccdbdec3c2a75b991879f9c5aee1f1
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-02-11 22:26:11 +0530

    Refactor user model to improve code consistency and add role to access token payload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
-import mongoose, {Schema} from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { env } from '../config/env.js';
 
 const userSchema = new Schema(
     {
@@ -9,85 +10,91 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             lowercase: true,
-            trim: true, 
-            index: true
+            trim: true,
+            index: true,
         },
         email: {
             type: String,
             required: true,
             unique: true,
             lowercase: true,
-            trim: true, 
-        },     
+            trim: true,
+        },
         fullName: {
             type: String,
             required: true,
-            trim: true, 
-            index: true
+            trim: true,
+            index: true,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
         },
         avatar: {
             type: String, // cloudinary url
-            required: true,
+            default: '',
         },
         coverImage: {
             type: String, // cloudinary url
+            default: '',
         },
         watchHistory: [
             {
                 type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
+                ref: 'Video',
+            },
         ],
         password: {
             type: String,
-            required: [true, 'Password is required']
+            required: [true, 'Password is required'],
         },
         refreshToken: {
-            type: String
-        }
+            type: String,
+        },
 
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign(
         {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName
+            fullName: this.fullName,
+            role: this.role,
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        env.accessTokenSecret,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: env.accessTokenExpiry,
         }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
+    );
+};
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
-        { 
-            _id: this._id,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            _id: this._id,
+        },
+        env.refreshTokenSecret,
+        {
+            expiresIn: env.refreshTokenExpiry,
         }
-    )
-}
+    );
+};
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model('User', userSchema);
