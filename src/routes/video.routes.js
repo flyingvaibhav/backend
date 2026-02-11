95ca36140726d8b2e7ea7feb543d477b19eece6f
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-02-11 22:22:49 +0530

    Add featuredVideos route to retrieve featured video content

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { listVideos, createVideo, incrementView } from '../controllers/video.controller.js';
+import { listVideos, featuredVideos, createVideo, incrementView } from '../controllers/video.controller.js';
 import { uploadVideoAndThumbnail } from '../middlewares/upload.js';
 
 const router = Router();
 
 router.get('/', listVideos);
+router.get('/featured', featuredVideos);
 router.post('/', uploadVideoAndThumbnail, createVideo);
 router.post('/:id/view', incrementView);
 
